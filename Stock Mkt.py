e3d54518ffda42ab01a9b5854a3f3b7480ea1a1d
mumbikairu <69993458+mumbikairu@users.noreply.github.com>
2021-11-05 13:00:23 -0400

    Add files via upload

---
+++ Stock Mkt.py
@@ -0,0 +1,14 @@
+# Raw Package
+import numpy as np
+import pandas as pd
+
+#Data Source
+import yfinance as yf
+
+#Data viz
+import plotly.graph_objs as go
+
+#Interval required 5 minutes
+data = yf.download(tickers='UBER', period='5d', interval='5m')
+#Print data
+data
