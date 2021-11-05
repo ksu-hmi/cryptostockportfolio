ca8ca0f3f28c977b2b6bc884e15f31d7ccde2372
Usen Usen <uusen@students.kennesaw.edu>
2021-11-05 00:45:36 -0400

    Update lookup_crypto.py

--- lookup_crypto.py
+++ lookup_crypto.py
@@ -13,6 +13,9 @@ from requests import Request, Session
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 import json
 
+import datetime
+from datetime import date, timedelta
+
 url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
 parameters = {
   'start':'1',
@@ -28,15 +31,20 @@ session = Session()
 session.headers.update(headers)
 
 try:
-  api_request = session.get(url, params=parameters)
-  api = json.loads(api_request.content)
-  print(api)
+    response = session.get(url, params=parameters)
+    data = json.loads(response.text)
+    #print(data)
 except (ConnectionError, Timeout, TooManyRedirects) as e:
-  print(e)
+   print(e)
 
-currencies = {'BTC','XRP','LUNA','SHIB','ETH','VET','ADA','CHZ','FET','MATIC'}
+currencies = ['BTC','XRP','LUNA','SHIB','ETH','VET','ADA','CHZ','FET','MATIC']
 
-for x in api:
+for x in data:
     for coin in currencies:
-        if coin == int(x['symbol']):
-            print(str(x['symbol']))
+        if coin == x['symbol']:
+            print(x['name'])
+        
+            print("${0:.2f}".format(float(x['price'])))
+            print("Circulating Supply: {0:.2f}".format(float(x['circulating_supply'])))
+            print("Total Supply: {0:.2f}".format(float(x['total_supply'])))
+            print("Rank: {0:.0f}".format(float(x['cmc_rank'])))
