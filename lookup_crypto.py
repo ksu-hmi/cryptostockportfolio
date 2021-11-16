32eb6a1cdd49871f0aa064424d2f00fc32e7870d
Usen Usen <uusen@students.kennesaw.edu>
2021-11-16 13:58:59 -0500

    Update lookup_crypto.py

--- lookup_crypto.py
+++ lookup_crypto.py
@@ -1,5 +1,5 @@
-#import requests
-#import json
+import requests
+import json
 
 #import os
 #os.system('cls')
@@ -9,42 +9,56 @@
 
 #print(api)
 
-from requests import Request, Session
-from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
-import json
+#from requests import Session
+#from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+#import json
+#import pprint
 
-import datetime
-from datetime import date, timedelta
+#from datetime import date, timedelta
 
-url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
-parameters = {
-  'start':'1',
-  'limit':'5000',
-  'convert':'USD'
-}
-headers = {
-  'Accepts': 'application/json',
-  'X-CMC_PRO_API_KEY': 'a6e2a4fb-fc21-43cc-8157-3f2f0eb1f6e9',
-}
+#url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
+#parameters = {
+  #'symbol':'BTC,XRP,LUNA,SHIB,ETH,VET,ADA,CHZ,FET,MATIC',
+  
+  #'convert':'USD'
+#}
+#headers = {
+  #'Accepts': 'application/json',
+  #'X-CMC_PRO_API_KEY': 'a6e2a4fb-fc21-43cc-8157-3f2f0eb1f6e9'
+#}
 
-session = Session()
-session.headers.update(headers)
+#session = Session()
+#session.headers.update(headers)
 
-try:
-    response = session.get(url, params=parameters)
-    data = json.loads(response.text)
+#try:
+    #response = session.get(url, params=parameters)
+    #data = json.loads(response.content)
     #print(data)
-except (ConnectionError, Timeout, TooManyRedirects) as e:
-   print(e)
+#except (ConnectionError, Timeout, TooManyRedirects) as e:
+   #print(e)
 
-currencies = ['BTC','XRP','LUNA','SHIB','ETH','VET','ADA','CHZ','FET','MATIC']
+#response = session.get(url, params=parameters)
+#data = (json.loads(response.text)['data']['2']['quote']['USD'])
+#pprint.pprint(data)
 
-for x in data:
+#currencies = ['BTC']
+#'XRP','LUNA','SHIB','ETH','VET','ADA','CHZ','FET','MATIC']
+
+api_request = requests.get("https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=a6e2a4fb-fc21-43cc-8157-3f2f0eb1f6e9&start=1&limit=5000&convert=USD")
+
+api = json.loads(api_request.content)
+
+currencies = ["BTC","XRP","LUNA","SHIB","ETH","VET","ADA","CHZ","FET"]
+
+
+for x in api['data']:
     for coin in currencies:
-        if coin == x['symbol']:
-            print(x['name'])
+      if coin == x["symbol"]:
+        print(x["symbol"])
+        print(x["name"])
+        print(x["quote"]["USD"]["price"])
         
-            print("${0:.2f}".format(float(x['price'])))
-            print("Circulating Supply: {0:.2f}".format(float(x['circulating_supply'])))
-            print("Total Supply: {0:.2f}".format(float(x['total_supply'])))
-            print("Rank: {0:.0f}".format(float(x['cmc_rank'])))
+        print("${0:.2f}".format(float(x["quote"]["USD"]["price"])))
+        print("Circulating Supply: {0:.2f}".format(float(x["circulating_supply"])))
+        print("Total Supply: {0:.2f}".format(float(x["total_supply"])))
+        print("Rank: {0:.0f}".format(float(x["cmc_rank"])))
