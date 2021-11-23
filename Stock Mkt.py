68628c52d0d1ba661c67095e0456baa959eb52b5
mumbikairu <69993458+mumbikairu@users.noreply.github.com>
2021-11-22 21:43:13 -0500

    Update Stock Mkt.py

--- Stock Mkt.py
+++ Stock Mkt.py
@@ -13,6 +13,39 @@ data = yf.download(tickers='UBER', period='5d', interval='5m')
 #Print data
 print(data)
 
+#declare figure
+fig = go.Figure()
+
+#Candlestick
+fig.add_trace(go.Candlestick(x=data.index,
+                open=data['Open'],
+                high=data['High'],
+                low=data['Low'],
+                close=data['Close'], name = 'market data'))
+
+# Add titles
+fig.update_layout(
+    title='Uber live share price evolution',
+    yaxis_title='Stock Price (USD per Shares)')
+
+# X-Axes
+fig.update_xaxes(
+    rangeslider_visible=True,
+    rangeselector=dict(
+        buttons=list([
+            dict(count=15, label="15m", step="minute", stepmode="backward"),
+            dict(count=45, label="45m", step="minute", stepmode="backward"),
+            dict(count=1, label="HTD", step="hour", stepmode="todate"),
+            dict(count=3, label="3h", step="hour", stepmode="backward"),
+            dict(step="all")
+        ])
+    )
+)
+
+#Show
+fig.show()
+
+
 def main_menu():
     #The home screen, by default the program always returns here
 
