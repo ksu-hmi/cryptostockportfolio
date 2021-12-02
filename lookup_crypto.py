f8ce09c18cf20522b5d3505da2e834b53c836385
Usen Usen <uusen@students.kennesaw.edu>
2021-12-01 21:52:52 -0500

    Update lookup_crypto.py
    create update buttons & a pie chart with matplotlib

--- lookup_crypto.py
+++ lookup_crypto.py
@@ -1,8 +1,15 @@
 import json
 import pprint
 from tkinter import *
+import matplotlib.pyplot as plt
 
 import requests
+
+def red_green(amount):
+  if amount >= 0:
+    return "green"
+  else:
+    return "red"
 
 root = Tk()
 
@@ -40,7 +47,7 @@ header_seven_day_change.grid(row=0, column=7, sticky =N+S+E+W)
 header_current_value = Label(root, text="Current Value", bg="white", font="Verdana 8 bold")
 header_current_value.grid(row=0, column=8, sticky =N+S+E+W)
 
-header_profit_loss_total = Label(root, text="Profit Loss Total", bg="silver", font = "Verdana 8 bold")
+header_profit_loss_total = Label(root, text="Profit/Loss Total", bg="silver", font = "Verdana 8 bold")
 header_profit_loss_total.grid(row=0, column=9, sticky =N+S+E+W)
 
 
@@ -106,8 +113,10 @@ def lookup():
   ]
   pprint.pprint("------------------------------------------------------------------------")
   portfolio_profit_loss = 0
+  total_current_value =0
   row_count = 1
-  total_current_value = 0
+  pie = []
+  pie_size = []
   for crypto in api['data']:
     for coin in my_crypto_portfolio:
       if coin["sym"] == crypto["symbol"]:
@@ -118,17 +127,19 @@ def lookup():
         portfolio_profit_loss += profit_loss
         profit_loss_per_coin = float(crypto["quote"]["USD"]["price"])- float(coin["price_paid_per"])
         total_current_value += current_value
+        pie.append(crypto["name"])
+        pie_size.append(coin["amount_owned"])
 
-        pprint.pprint(crypto["symbol"])
-        pprint.pprint(crypto["name"])
-        pprint.pprint(" Current Price: ${0:.2f}".format(float(crypto["quote"]["USD"]["price"])))
-        pprint.pprint(" Profit/Loss Per Coin: ${0:.2f}".format(float(profit_loss_per_coin)))
-        pprint.pprint(" Circulating Supply: {0:.2f}".format(float(crypto["circulating_supply"])))
-        pprint.pprint(" Total Supply: {0:.2f}".format(float(crypto["total_supply"])))
-        pprint.pprint(" Total Paid: ${0:.2f}".format(float(current_value)))
-        pprint.pprint(" Rank: {0:.0f}".format(float(crypto["cmc_rank"])))
-        pprint.pprint(" Profit/Loss: ${0:.2f}".format(float(profit_loss)))
-        pprint.pprint("------------------------------------------------------------------------")
+        #pprint.pprint(crypto["symbol"])
+        #pprint.pprint(crypto["name"])
+        #pprint.pprint(" Current Price: ${0:.2f}".format(float(crypto["quote"]["USD"]["price"])))
+        #pprint.pprint(" Profit/Loss Per Coin: ${0:.2f}".format(float(profit_loss_per_coin)))
+        #pprint.pprint(" Circulating Supply: {0:.2f}".format(float(crypto["circulating_supply"])))
+        #pprint.pprint(" Total Supply: {0:.2f}".format(float(crypto["total_supply"])))
+        #pprint.pprint(" Total Paid: ${0:.2f}".format(float(current_value)))
+        #pprint.pprint(" Rank: {0:.0f}".format(float(crypto["cmc_rank"])))
+        #pprint.pprint(" Profit/Loss: ${0:.2f}".format(float(profit_loss)))
+        #pprint.pprint("------------------------------------------------------------------------")
         
         #-------------------Note: This step can also be done with a loop-------
         name = Label(root, text=crypto["name"], bg="white")
@@ -143,28 +154,50 @@ def lookup():
         price_paid = Label(root, text="${0:.2f}".format(float(coin["price_paid_per"])), bg="silver")
         price_paid.grid(row=row_count, column=3, sticky =N+S+E+W)
 
-        profit_loss_per = Label(root, text="${0:.2f}".format(float(profit_loss_per_coin)), bg="white")
+        profit_loss_per = Label(root, text="${0:.2f}".format(float(profit_loss_per_coin)), bg="white", fg= red_green(float(profit_loss_per_coin)))
         profit_loss_per.grid(row=row_count, column=4, sticky =N+S+E+W)
 
-        #one_hr_change = Label(root, text="{0:.2f}%".format(float(crypto["percent_change_1h"])), bg="silver")
+        #one_hr_change = Label(root, text="{0:.2f}%".format(float(crypto["percent_change_1h"])), bg="silver", fg= red_green(float(crypto["percent_change_1h"])))
         #one_hr_change.grid(row=row_count, column=5, sticky =N+S+E+W)
 
-        #twenty4_hr_change = Label(root, text="{0:.2f}%".format(float(crypto["percent_change_24h"])), bg="white")
+        #twenty4_hr_change = Label(root, text="{0:.2f}%".format(float(crypto["percent_change_24h"])), bg="white", fg= red_green(float(crypto["percent_change_24h"])))
         #twenty4_hr_change.grid(row=row_count, column=6, sticky =N+S+E+W)
 
-        #seven_day_change = Label(root, text="{0:.2f}%".format(float(crypto["percent_change_7d"])), bg="silver")
+        #seven_day_change = Label(root, text="{0:.2f}%".format(float(crypto["percent_change_7d"])), bg="silver", fg= red_green(float(crypto["percent_change_7d"])))
         #seven_day_change.grid(row=row_count, column=7, sticky =N+S+E+W)
 
         current_value = Label(root, text="${0:.2f}".format(float(current_value)), bg="white")
         current_value.grid(row=row_count, column=8, sticky =N+S+E+W)
 
-        profit_loss_total = Label(root, text="${0:.2f}".format(float(profit_loss)), bg="silver")
+        profit_loss_total = Label(root, text="${0:.2f}".format(float(profit_loss)), bg="silver", fg= red_green(float(profit_loss)))
         profit_loss_total.grid(row=row_count, column=9, sticky =N+S+E+W)
 
         row_count +=1
+  
+  portfolio_profits = Label(root, text = "Total Portfolio Profit/Loss: ${0:2f}".format(float(portfolio_profit_loss)), font = "Verdana 8 bold", fg=red_green(float(portfolio_profit_loss)))
+  portfolio_profits.grid(row=row_count,column =0, sticky = W, padx = 10, pady = 10)
+
+  root.title("Crypto Currency Portfolio - Porfolio Value: ${0:.2f}".format(float(total_current_value)))
+
+  api = ""
+  update_button = Button(root, text = "Update Prices", command = lookup)
+  update_button.grid(row=row_count, column =9, sticky = E+S, padx= 10, pady =10)
+
+
+# Data to plot
+  def graph(pie, pie_size):
+    labels = pie
+    sizes = pie_size
+    colors = ['yellowgreen', 'gold','lightskyblue','lightcoral', 'red', 'brown', 'blue','orange','pink','green']
+    patches, texts= plt.pie(sizes, colors= colors, shadow= True, startangle=90)
+    plt.legend(patches, labels, loc="best")
+    plt.axis('equal')
+    plt.tight_layout()
+    plt.show()
+  
+  graph_button = Button(root, text="Pie Chart", command= lambda: graph(pie, pie_size))
+  graph_button.grid(row=row_count, column =8, sticky = E+S, padx=10, pady=10)
+  
 
 lookup()
 root.mainloop()
-
-
-
